9b3b26c2dea0dd04516c7e7cb1e525f1e62c5097
Arun Kumar <arun.kumar@ziroh.com>
2024-12-04 09:41:35 +0530

    #36 Text Editor Bootcamp.

---
+++ SwiftulThinkingBootcamp/Bootcamps/ToogleBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ToogleBootcamp.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Arun Kumar on 04/12/24.
+//
+
+import SwiftUI
+
+struct ToogleBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToogleBootcamp()
+}
