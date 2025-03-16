18178c9ca33cd2c651eb882a494ad7aca835ac49
Arun Kumar <kumar.arun.ynr@gmail.com>
2025-03-16 11:00:36 +0530

    Grid bootcamp.

--- SwiftulThinkingBootcamp/Bootcamps/GridBootcamp.swift
+++ SwiftulThinkingBootcamp/Bootcamps/GridBootcamp.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct GridBootcamp: View {
-    var body: some View {
-        Grid {
-            
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+
+
+        var body: some View {
+             ScrollView {
+                 LazyVGrid(columns: columns) {
+                     ForEach(0x1f600...0x1f64f, id: \.self) { value in
+                         Text(String(format: "%x", value))
+                         Text(emoji(value))
+                             .font(.largeTitle)
+                     }
+                 }
+             }
         }
-    }
+
+
+        private func emoji(_ value: Int) -> String {
+            guard let scalar = UnicodeScalar(value) else { return "?" }
+            return String(Character(scalar))
+        }
 }
 
 #Preview {
